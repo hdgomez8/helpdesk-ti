934e51d66f418edd0ec2b39307ba9557a2b3dbd1
Hector David Gomez Pertuz <hdgomez8@hotmail.com>
2023-11-01 15:23:00 -0500

    agregando archivo Settings

---
+++ config/settings.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'DB_HOST' => 'localhost',
+    'DB_USERNAME' => 'root',
+    'DB_PASSWORD' => 'Orion1225',
+    'DB_DATABASE' => 'ti-ticket-pruebas',
+    'APP_DEBUG' => true,
+    'DIRECCION_PROYECTO'=> 'http://192.168.1.128:8088/helpdesk-ti/',
+];
