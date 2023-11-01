11f2f657e5d5bf483dd0638a979edbcb95ebdf8e
unknown <Administrator@colsalud.com>
2023-11-01 15:27:36 -0500

    gitignore excluyendo settings

--- config/settings.php
+++ config/settings.php
@@ -4,7 +4,7 @@ return [
     'DB_HOST' => 'localhost',
     'DB_USERNAME' => 'root',
     'DB_PASSWORD' => 'Orion1225',
-    'DB_DATABASE' => 'ti-ticket-pruebas',
+    'DB_DATABASE' => 'ticket',
     'APP_DEBUG' => true,
-    'DIRECCION_PROYECTO'=> 'http://192.168.1.128:8088/helpdesk-ti/',
+    'DIRECCION_PROYECTO'=> 'http://192.168.1.192:8080/helpdesk/',
 ];
